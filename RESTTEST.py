94ba9117325aacc2198462fb34ff0acceffa083b
Martian1337 <73861544+Martian1337@users.noreply.github.com>
2022-12-29 12:51:35 -0600

    Update RESTTEST.py
    Added more testing methods with the ability to run tests from a file

--- RESTTEST.py
+++ RESTTEST.py
@@ -1,11 +1,10 @@
-# Import the requests module
 import requests
 
 # Define the list of HTTP methods to test
 methods = ['GET', 'POST', 'PUT', 'DELETE']
 
 # Prompt the user for the URL of the API endpoint
-url = input('Enter the URL of the API endpoint: ')
+url = input('Enter the URL of the API endpoint (or type "list" to test a list of URLs from a text file): ')
 
 # Define the headers that will be sent with each request
 headers = {
@@ -13,32 +12,65 @@ headers = {
     'Authorization': 'Bearer <ACCESS_TOKEN>',
 }
 
+
 # Define the payload that will be sent with POST and PUT requests
 payload = {
     'key1': 'value1',
     'key2': 'value2',
 }
 
+# Test a single URL if the user entered one
 # Iterate over each method and send a request to the API
-for method in methods:
-    try:
-        # Send request with specified method
-        if method == 'GET':
-            response = requests.get(url, headers=headers)
-        elif method == 'POST':
-            response = requests.post(url, json=payload, headers=headers)
-        elif method == 'PUT':
-            response = requests.put(url, json=payload, headers=headers)
-        elif method == 'DELETE':
-            response = requests.delete(url, headers=headers)
-        else:
-            raise ValueError('Invalid HTTP method')
+if url != 'list':
+    for method in methods:
+        try:
+            # Send request with specified method
+            if method == 'GET':
+                response = requests.get(url, headers=headers)
+            elif method == 'POST':
+                response = requests.post(url, json=payload, headers=headers)
+            elif method == 'PUT':
+                response = requests.put(url, json=payload, headers=headers)
+            elif method == 'DELETE':
+                response = requests.delete(url, headers=headers)
+            else:
+                raise ValueError('Invalid HTTP method')
 
-        # Print the response text
-        print(method + ':', response.text)
-    except requests.exceptions.RequestException as e:
-        # Print the error message if the request failed
-        print(method + ':', e)
+            # Print the response text
+            print(method + ':', response.text)
+        except requests.exceptions.RequestException as e:
+            # Print the error message if the request failed
+            print(method + ':', e)
+# Read the list of URLs from the text file and test them if the user entered "list"
+else:
+    # Read the list of URLs from the text file
+    with open('urls.txt', 'r') as f:
+        urls = f.readlines()
+
+    # Iterate over each URL and test each HTTP method
+    for url in urls:
+        for method in methods:
+            try:
+                # Send request with specified method
+                if method == 'GET':
+                    response = requests.get(url, headers=headers)
+                elif method == 'POST':
+                    response = requests.post(url, json=payload, headers=headers)
+                elif method == 'PUT':
+                    response = requests.put(url, json=payload, headers=headers)
+                elif method == 'DELETE':
+                    response = requests.delete(url, headers=headers)
+                else:
+                    raise ValueError('Invalid HTTP method')
+
+                # Print the response text
+                print(method + ':', response.text)
+            except requests.exceptions.RequestException as e:
+                # Print the error message if the request failed
+                print(method + ':', e)
+
+
+# GET testing
 
 # Test sending a request with an invalid username and password
 try:
@@ -52,7 +84,9 @@ try:
     response = requests.get(url, headers=headers)
     print('GET without credentials:', response.text)
 except requests.exceptions.RequestException as e:
-    print('GET without credentials:', e)# Test sending a request with an invalid access token
+    print('GET without credentials:', e)
+    
+    # Test sending a request with an invalid access token
 try:
     headers_invalid_token = headers.copy()
     headers_invalid_token['Authorization'] = 'Bearer <INVALID_ACCESS_TOKEN>'
@@ -70,6 +104,10 @@ try:
 except requests.exceptions.RequestException as e:
     print('GET without token:', e)
 
+
+
+# POST Testing
+
 # Test sending a request with a tampered payload
 try:
     payload_tampered = payload.copy()
@@ -79,4 +117,114 @@ try:
 except requests.exceptions.RequestException as e:
     print('POST with tampered payload:', e)
 
+# Test sending a request with an invalid access token
+try:
+    headers_invalid_token = headers.copy()
+    headers_invalid_token['Authorization'] = 'Bearer <INVALID_ACCESS_TOKEN>'
+    response = requests.post(url, json=payload, headers=headers_invalid_token)
+    print('POST with invalid token:', response.text)
+except requests.exceptions.RequestException as e:
+    print('POST with invalid token:', e)
 
+# Test sending a request without an access token
+try:
+    headers_no_token = headers.copy()
+    headers_no_token.pop('Authorization', None)
+    response = requests.post(url, json=payload, headers=headers_no_token)
+    print('POST without token:', response.text)
+except requests.exceptions.RequestException as e:
+    print('POST without token:', e)
+
+# Test sending a request with an invalid payload
+try:
+    payload_invalid = {'invalid_key': 'invalid_value'}
+    response = requests.post(url, json=payload_invalid, headers=headers)
+    print('POST with invalid payload:', response.text)
+except requests.exceptions.RequestException as e:
+    print('POST with invalid payload:', e)
+
+# Test sending a request with a missing required field in the payload
+try:
+    payload_missing_field = {'key1': 'value1'}
+    response = requests.post(url, json=payload_missing_field, headers=headers)
+    print('POST with missing field in payload:', response.text)
+except requests.exceptions.RequestException as e:
+    print('POST with missing field in payload:', e)
+
+# Test sending a request with an invalid URL
+try:
+    response = requests.post('http://invalid.url', json=payload, headers=headers)
+    print('POST with invalid URL:', response.text)
+except requests.exceptions.RequestException as e:
+    print('POST with invalid URL:', e)
+
+# Test sending a request with a XSS payload in key value
+try:
+    payload_malicious = {'key1': '<SCRIPT>alert("XSS")</SCRIPT>'}
+    response = requests.post(url, json=payload_malicious, headers=headers)
+    print('POST with malicious payload:', response.text)
+except requests.exceptions.RequestException as e:
+    print('POST with malicious payload:', e)
+
+# Test sending a request with a payload that has been padded to try and bypass input validation
+try:
+    payload_padded = payload.copy()
+    payload_padded['key1'] = payload_padded['key1'] + 'a' * 100
+    response = requests.post(url, json=payload_padded, headers=headers)
+    print('POST with padded payload:', response.text)
+except requests.exceptions.RequestException as e:
+    print('POST with padded payload:', e)
+
+# Test sending a request with a payload that has been truncated to try and bypass input validation
+try:
+    payload_truncated = payload.copy()
+    payload_truncated['key1'] = payload_truncated['key1'][:5]
+    response = requests.post(url, json=payload_truncated, headers=headers)
+    print('POST with truncated payload:', response.text)
+except requests.exceptions.RequestException as e:
+    print('POST with truncated payload:', e)
+
+
+
+# PUT Testing
+
+# Test sending a request with an invalid access token
+try:
+    headers_invalid_token = headers.copy()
+    headers_invalid_token['Authorization'] = 'Bearer <INVALID_ACCESS_TOKEN>'
+    response = requests.put(url, json=payload, headers=headers_invalid_token)
+    print('PUT with invalid token:', response.text)
+except requests.exceptions.RequestException as e:
+    print('PUT with invalid token:', e)
+
+# Test sending a request without an access token
+try:
+    headers_no_token = headers.copy()
+    headers_no_token.pop('Authorization', None)
+    response = requests.put(url, json=payload, headers=headers_no_token)
+    print('PUT without token:', response.text)
+except requests.exceptions.RequestException as e:
+    print('PUT without token:', e)
+
+# Test sending a request with an invalid payload
+try:
+    payload_invalid = {'invalid_key': 'invalid_value'}
+    response = requests.put(url, json=payload_invalid, headers=headers)
+    print('PUT with invalid payload:', response.text)
+except requests.exceptions.RequestException as e:
+    print('PUT with invalid payload:', e)
+
+#Test sending a request with a missing required field in the payload
+try:
+    payload_missing_field = {'key1': 'value1'}
+    response = requests.put(url, json=payload_missing_field, headers=headers)
+    print('PUT with missing field in payload:', response.text)
+except requests.exceptions.RequestException as e:
+    print('PUT with missing field in payload:', e)
+
+# Test sending a request with an invalid URL
+try:
+    response = requests.put('http://invalid.url', json=payload, headers=headers)
+    print('PUT with invalid URL:', response.text)
+except requests.exceptions.RequestException as e:
+    print('PUT with invalid URL:', e)
